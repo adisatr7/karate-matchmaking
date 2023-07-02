67ff9920317f3eb32b5e58534ee00b170c72b5bb
adisatr7 <adisatr7@gmail.com>
2023-07-02 10:18:06 +0700

    Stylized under construction page

--- src/screens/UnderConstructionScreen.tsx
+++ src/screens/UnderConstructionScreen.tsx
@@ -1,9 +1,9 @@
 export default function UnderConstructionScreen() {
   return (
-    <div className="flex flex-col items-center justify-center bg-stone-800">
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-stone-800">
       <h1
         onClick={() => window.history.back()}
-        className="text-white hover:underline font-quicksand text-heading">
+        className="text-white hover:underline font-quicksand text-heading hover:cursor-pointer">
         Under Construction
       </h1>
     </div>
