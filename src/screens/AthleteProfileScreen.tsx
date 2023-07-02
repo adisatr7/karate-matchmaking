6f520cb547d9a5bba7364ce8033147a0432262ab
adisatr7 <adisatr7@gmail.com>
2023-07-02 16:18:56 +0700

    Added WIP performance section to athlete profile

--- src/screens/AthleteProfileScreen.tsx
+++ src/screens/AthleteProfileScreen.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import MainLayout from "../components/MainLayout"
 import Team from "../data/classes/Team"
 import Button from "../components/Button"
+import NumericDisplay from "../components/NumericDisplay"
 
 
 type ParamsType = {
@@ -13,8 +14,8 @@ type ParamsType = {
 export default function AthleteDetailScreen() {
   const params = useParams<ParamsType>()
   const [currentAthlete, setCurrentAthlete] = useState<Athlete | undefined>()
-
   const [currentTeam, setCurrentTeam] = useState<Team | undefined>()
+  const [winrate, setWinrate] = useState<number>(0)
   
   /**
    * Fetch the athlete data from the `athletes` directory
@@ -26,7 +27,11 @@ export default function AthleteDetailScreen() {
     
     // Load the athlete data from the `athletes` directory and set it to the state
     while (currentAthlete === undefined) {
-      await Athlete.load(id!).then(athleteData => setCurrentAthlete(athleteData))
+      await Athlete.load(id!)
+        .then(athleteData => setCurrentAthlete(athleteData))
+      
+      await currentAthlete!.calculateWinRate()
+        .then(winrate => setWinrate(winrate))
     }
   }
     
@@ -58,30 +63,55 @@ export default function AthleteDetailScreen() {
       <div className="flex flex-row gap-4 mr-[14px] items-start mt-[12px]">
 
         {/* Profile Picture */}
-        <img src={currentAthlete?.getImageUrl()} className="bg-cover h-full w-[180px] bg-primary-opaque border border-primary-opaque rounded-md"/>
+        <div
+          className="bg-cover h-full w-[500px] bg-primary-opaque border border-primary-opaque rounded-md">
+          <img 
+            src={currentAthlete?.getImageUrl()}/>
+        </div>
 
-        {/* Bio */}
-        <div className="flex flex-col h-full w-fit gap-[2px]">
-
-          {/* BIo label */}
-          <h2 className="text-white font-quicksand text-subheading">Bio</h2>
+        {/* Bio section */}
+        <div className="flex flex-col h-full w-full gap-[2px] mb-[6px]">
+          <h2 className={subheadingTextStyle}>Bio</h2>
           
           {/* Current team text */}
-          <div className="flex flex-row items-center gap-[8px] h-fit"><p className={bioTextStyle}>Tim saat ini:</p>
+          <div className="flex flex-row items-center gap-[8px] h-fit"><p className={captionTextStyle}>Tim saat ini:</p>
             <Link to={`/team/${currentTeam?.getId()}`}>
-              <p className={`hover:underline font-bold ${bioTextStyle}`}>{currentTeam?.getTeamName()}</p>
+              <p className={`hover:underline font-bold ${captionTextStyle}`}>{currentTeam?.getTeamName()}</p>
             </Link>
           </div>
 
-          <p className={bioTextStyle}>Jenis kelamin: {currentAthlete?.getGender() === "m" ? "Laki-laki" : "Perempuan"}</p> 
+          <p className={captionTextStyle}>Jenis kelamin: {currentAthlete?.getGender() === "m" ? "Laki-laki" : "Perempuan"}</p> 
           {/* TODO: Restructure the Athlete class AGAIN! */}
-          <p className={bioTextStyle}>Usia: {currentAthlete?.getAge()} tahun</p>
-          <p className={bioTextStyle}>Tempat & tanggal lahir: [NOT IMPLEMENTED]</p>
-          <p className={bioTextStyle}>Berat badan: {currentAthlete?.getWeight()} kg</p>
-          <p className={bioTextStyle}>Tinggi badan: [NOT IMPLEMENTED] cm</p>
+          <p className={captionTextStyle}>Usia: {currentAthlete?.getAge()} tahun</p>
+          <p className={captionTextStyle}>Tempat & tanggal lahir: [NOT IMPLEMENTED]</p>
+          <p className={captionTextStyle}>Berat badan: {currentAthlete?.getWeight()} kg</p>
+          <p className={captionTextStyle}>Tinggi badan: [NOT IMPLEMENTED] cm</p>
 
-          <Button label="PERBARUI BIODATA" className="text-caption"/>
-          
+          {/* Edit profile button */}
+          <Button label="PERBARUI BIODATA" className="text-caption w-[280px] mt-[10px]"/>
+        </div>
+
+        {/* Performance section */}
+        <div className="flex flex-col w-full h-fit">
+          <h2 className={subheadingTextStyle}>Performa</h2>
+            
+          <div className="flex flex-row">
+
+            {/* Numeric stats */}
+            <div className="flex flex-col w-full">
+              
+              <NumericDisplay label="Yuko"/>
+              <NumericDisplay label="Wazari"/>
+              <NumericDisplay label="Ippon"/>
+              <NumericDisplay label="Winrate" value={`${winrate}%`}/>
+            </div>
+
+            {/* Radar chart */}
+            <div className="flex flex-col w-full">  
+              {/* TODO: Put <RadarChart/> component here */}
+              <div className="flex flex-row items-center justify-center text-white rounded-full bg-opacity-80 h-[200px] w-[200px] bg-dark-glass">Placeholder</div>
+            </div>
+          </div>
         </div>
 
       </div>
@@ -89,4 +119,5 @@ export default function AthleteDetailScreen() {
   )
 }
 
-const bioTextStyle = "text-white font-quicksand text-caption"
+const captionTextStyle = "text-white font-quicksand text-caption"
+const subheadingTextStyle = "text-white font-quicksand text-subheading"
